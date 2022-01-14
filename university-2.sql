841bfe3bab7f2d650b7f9bfb516441125d7f4ff3
luirusso <luirusso_@hotmail.it>
2022-01-14 17:05:46 +0100

    Completed second exercise

---
+++ university-2.sql
@@ -0,0 +1,47 @@
+--GROUP BY
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(`id`) AS 'Numero iscritti per anno', YEAR(`enrolment_date`) AS 'Anno'
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(`id`) AS 'Numero insegnanti', `office_address` AS 'Edificio in comune'
+FROM `teachers` 
+GROUP BY `office_address`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT AVG(`vote`) AS 'Media voto', `exam_id` AS 'ID Esame'
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(`id`) AS 'Numero corsi', `department_id` AS 'Dipartimento'
+FROM `degrees`
+GROUP BY `department_id`;
+
+-- JOIN
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT * 
+FROM `students`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT * 
+FROM `degrees`
+INNER JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT * 
+FROM `course_teacher`
+WHERE `teacher_id` = 44;
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT * 
+FROM `students`
+INNER JOIN `degrees` ON `degrees`.`id` = `students`.`degree_id`
+ORDER BY `students`.`name`, `students`.`surname` ASC;
